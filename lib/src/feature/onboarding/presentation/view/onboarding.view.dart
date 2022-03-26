5eb1dfd780e3b59976b7791ab0f233230df3b582
musahinist <sahinmusa1@gmail.com>
2022-03-27 00:23:18 +0300

    refactor: onboarding page

--- lib/src/feature/onboarding/presentation/view/onboarding.view.dart
+++ lib/src/feature/onboarding/presentation/view/onboarding.view.dart
@@ -38,74 +38,122 @@ class _OnboardingViewState extends State<OnboardingView>
           TabBarView(
             controller: _tabController,
             children: [
-              Container(
-                color: Colors.purple,
-                child: const Center(
-                  child: Text('Page 1'),
-                ),
+              Column(
+                //  alignment: Alignment.topCenter,
+                children: [
+                  Transform.scale(
+                    scale: 1.4,
+                    child: Container(
+                      height: 240,
+                      transform: Matrix4.translationValues(0, -40, 0),
+                      decoration: BoxDecoration(
+                        color: Colors.indigo[900],
+                        borderRadius: BorderRadius.vertical(
+                            bottom: Radius.elliptical(
+                                MediaQuery.of(context).size.width, 160)),
+                      ),
+                    ),
+                  ),
+                  const Center(
+                    child: Text('Well Designed Template'),
+                  ),
+                ],
               ),
-              Container(
-                color: Colors.amber,
-                child: const Center(
-                  child: Text('Page 2'),
-                ),
+              Column(
+                //  alignment: Alignment.topCenter,
+                children: [
+                  Transform.scale(
+                    scale: 1.4,
+                    child: Container(
+                      height: 240,
+                      transform: Matrix4.translationValues(0, -40, 0),
+                      decoration: BoxDecoration(
+                        color: Colors.indigo[900],
+                        borderRadius: BorderRadius.vertical(
+                            bottom: Radius.elliptical(
+                                MediaQuery.of(context).size.width, 160)),
+                      ),
+                    ),
+                  ),
+                  const Center(
+                    child: Text('East to Export and Share'),
+                  ),
+                ],
               ),
-              Container(
-                color: Colors.blue,
-                child: const Center(
-                  child: Text('Page 3'),
-                ),
+              Column(
+                //  alignment: Alignment.topCenter,
+                children: [
+                  Transform.scale(
+                    scale: 1.4,
+                    child: Container(
+                      height: 240,
+                      transform: Matrix4.translationValues(0, -40, 0),
+                      decoration: BoxDecoration(
+                        color: Colors.indigo[900],
+                        borderRadius: BorderRadius.vertical(
+                            bottom: Radius.elliptical(
+                                MediaQuery.of(context).size.width, 160)),
+                      ),
+                    ),
+                  ),
+                  const Center(
+                    child: Text('Easy to Create and Edit'),
+                  ),
+                ],
               ),
             ],
           ),
           SafeArea(
             child: Align(
               alignment: Alignment.topRight,
-              child: TextButton.icon(
-                style: TextButton.styleFrom(
-                  primary: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextButton.icon(
+                  style: TextButton.styleFrom(
+                    primary: Colors.white,
+                  ),
+                  label: const Text('Skip'),
+                  icon: const Icon(Icons.arrow_forward),
+                  onPressed: () {
+                    context.router.replaceAll([const SignInRoute()]);
+                  },
                 ),
-                label: const Text('Skip'),
-                icon: const Icon(Icons.arrow_forward),
-                onPressed: () {
-                  context.router.replaceAll([const SignInRoute()]);
-                },
               ),
             ),
           ),
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Visibility(
-                    visible: _tabController.index > 0,
-                    child: IconButton(
-                      icon: const Icon(Icons.arrow_back),
-                      onPressed: () {
-                        _tabController.animateTo(_tabController.index - 1);
-                      },
-                    ),
-                  ),
-                  Visibility(
-                    visible: _tabController.index < 2,
-                    child: IconButton(
-                      icon: const Icon(Icons.arrow_forward),
-                      onPressed: () {
-                        _tabController.animateTo(_tabController.index + 1);
-                      },
-                    ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 24.0),
-                child: TabPageSelector(
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TabPageSelector(
                   controller: _tabController,
                 ),
-              ),
-            ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Visibility(
+                      visible: _tabController.index > 0,
+                      child: IconButton(
+                        icon: const Icon(Icons.arrow_back),
+                        onPressed: () {
+                          _tabController.animateTo(_tabController.index - 1);
+                        },
+                      ),
+                    ),
+                    Visibility(
+                      visible: _tabController.index < 2,
+                      child: IconButton(
+                        icon: const Icon(Icons.arrow_forward),
+                        onPressed: () {
+                          _tabController.animateTo(_tabController.index + 1);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
