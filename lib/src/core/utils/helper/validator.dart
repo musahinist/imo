bffa65b0dc3caf64b67ed33310fb27f6dc48786e
musahinist <sahinmusa1@gmail.com>
2022-03-29 00:07:02 +0300

    wip: add ykn tckn vlidator

--- lib/src/core/utils/helper/validator.dart
+++ lib/src/core/utils/helper/validator.dart
@@ -17,6 +17,150 @@ class Validators {
   static isValidName(String name) {
     return name.isNotEmpty;
   }
+
+  String? email(String value) {
+    const Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    // r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$";
+    // r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    // r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
+    ///TODO: Refactor the below
+    final RegExp regex = RegExp(pattern as String);
+    if (!regex.hasMatch(value.trim())) {
+      return "labels.validator.email";
+    } else {
+      return null;
+    }
+  }
+
+  String? password(String value) {
+    const Pattern pattern = r'^.{6,}$';
+    final RegExp regex = RegExp(pattern.toString());
+    if (!regex.hasMatch(value.trim())) {
+      return "labels.validator.password";
+    } else {
+      return null;
+    }
+  }
+
+  String? name(String value) {
+    const Pattern pattern = r"^[A-Z İĞÜŞÖÇ]+$"; //r"^[a-zA-ZğüşöçıİĞÜŞÖÇ]+$";
+    final RegExp regex = RegExp(pattern.toString());
+    if (!regex.hasMatch(value.trim())) {
+      return "labels.validator.name";
+    } else {
+      return null;
+    }
+  }
+
+  String? number(String value) {
+    const Pattern pattern = r'^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$';
+    final RegExp regex = RegExp(pattern.toString());
+    if (!regex.hasMatch(value.trim())) {
+      return "labels.validator.number";
+    } else {
+      return null;
+    }
+  }
+
+  String? numeric(String value) {
+    const Pattern pattern = r'^[0-9]';
+    final RegExp regex = RegExp(pattern.toString());
+    if (!regex.hasMatch(value.trim())) {
+      return "labels.validator.number";
+    } else {
+      return null;
+    }
+  }
+
+  String? phone(String value) {
+    // const Pattern pattern = r'^(?:[+0]9)?[0-9]{7,12}$';
+    const Pattern pattern = r'^[0-9]{7,12}$';
+    final RegExp regex = RegExp(pattern.toString());
+    if (!regex.hasMatch(value.trim())) {
+      return "labels.validator.phone";
+    } else {
+      return null;
+    }
+  }
+
+  String? amount(String value) {
+    const Pattern pattern = r'^\d+$';
+    final RegExp regex = RegExp(pattern.toString());
+    if (!regex.hasMatch(value.trim())) {
+      return "labels.validator.amount";
+    } else if (int.parse(value.trim()) <= 0) {
+      return " labels.validator.greaterThanZero";
+    } else if (value.length >= 18) {
+      return "labels.validator.tooLongNumber";
+    } else {
+      return null;
+    }
+  }
+
+  String? notEmpty(String value) {
+    const Pattern pattern = r'^\S+$';
+    final RegExp regex = RegExp(pattern.toString());
+    if (!regex.hasMatch(value.trim())) {
+      return "labels.validator.notEmpty";
+    } else {
+      return null;
+    }
+  }
+
+  String? requiredField(String value) {
+    if (value == null || value.isEmpty) {
+      return "labels.validator.notEmpty";
+    } else {
+      return null;
+    }
+  }
+
+  String? tcknNumber(String value) {
+    const Pattern pattern = r'^[1-8][0-9]{10}$';
+    final RegExp regex = RegExp(pattern.toString());
+    if (!regex.hasMatch(value.trim())) {
+      return "labels.validator.tckn";
+    } else {
+      return yknTcknNumberAlgoritm(regex, value);
+    }
+  }
+
+  String? yknNumber(String value) {
+    const Pattern pattern = r'^[9-9][0-9]{10}$';
+    final RegExp regex = RegExp(pattern.toString());
+    if (!regex.hasMatch(value.trim())) {
+      return "labels.validator.ykn";
+    } else {
+      return yknTcknNumberAlgoritm(regex, value);
+    }
+  }
+
+  String? yknTcknNumberAlgoritm(RegExp regex, String value) {
+    var numberList = value.split("");
+    var totalFirst10 = 0;
+    for (var i = 0; i < 10; i++) {
+      totalFirst10 += int.tryParse(numberList[i])!;
+    }
+    if ((totalFirst10 % 10).toString() == numberList[10]) {
+      var totalY1 = 0;
+      var totalY2 = 0;
+      for (var i = 0; i < 9; i += 2) {
+        totalY1 += int.tryParse(numberList[i])!;
+      }
+      for (var i = 1; i < 9; i += 2) {
+        totalY2 += int.tryParse(numberList[i])!;
+      }
+      if (((totalY1 * 7 - totalY2) % 10).toString() == numberList[9]) {
+        return null;
+      }
+      print(totalFirst10);
+      print(totalY1);
+      print(totalY2);
+      return "labels.validator.tcknAndYkn";
+    }
+    return "labels.validator.tcknAndYkn";
+  }
 }
 
 /// Regex Validator
