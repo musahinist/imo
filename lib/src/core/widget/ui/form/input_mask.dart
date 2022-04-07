bfdff46ecdf068a4233adda23b0cefa4847afa2f
musahinist <sahinmusa1@gmail.com>
2022-04-07 19:29:53 +0300

    add: date input mask

---
+++ lib/src/core/widget/ui/form/input_mask.dart
@@ -0,0 +1,32 @@
+import 'dart:math';
+
+import 'package:flutter/services.dart';
+
+class DateInputFormatter extends TextInputFormatter {
+  static const _maxChars = 8;
+
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    var text = _format(newValue.text, '/');
+    return newValue.copyWith(text: text, selection: updateCursorPosition(text));
+  }
+
+  String _format(String value, String seperator) {
+    value = value.replaceAll(seperator, '');
+    var newString = '';
+
+    for (int i = 0; i < min(value.length, _maxChars); i++) {
+      newString += value[i];
+      if ((i == 1 || i == 3) && i != value.length - 1) {
+        newString += seperator;
+      }
+    }
+
+    return newString;
+  }
+
+  TextSelection updateCursorPosition(String text) {
+    return TextSelection.fromPosition(TextPosition(offset: text.length));
+  }
+}
