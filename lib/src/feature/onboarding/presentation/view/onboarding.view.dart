1f3cff882c3f61957b8a7109ab4f1fdd7e5f7f25
musahinist <sahinmusa1@gmail.com>
2022-04-09 04:01:37 +0300

    refactor: onboarding view

--- lib/src/feature/onboarding/presentation/view/onboarding.view.dart
+++ lib/src/feature/onboarding/presentation/view/onboarding.view.dart
@@ -1,161 +1,96 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../../../router/router.dart';
+import '../bloc/auth/auth_bloc.dart';
+import '../bloc/login/login_bloc.dart';
+import '../widget/loading_button.widget.dart';
 
-class OnboardingView extends StatefulWidget {
-  const OnboardingView({Key? key}) : super(key: key);
-  static const routeName = '/onboarding';
-
+class SignInView extends StatelessWidget {
+  const SignInView({Key? key}) : super(key: key);
+  static const String routeName = '/signIn';
   @override
-  State<OnboardingView> createState() => _OnboardingViewState();
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) =>
+          LoginBloc(context.read(), context.read(), context.read()),
+      child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: const _SignInBody()),
+    );
+  }
 }
 
-class _OnboardingViewState extends State<OnboardingView>
-    with SingleTickerProviderStateMixin {
-  late TabController _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: 3, vsync: this);
-    _tabController.addListener(() {
-      setState(() {});
-      // if (_tabController.index == 2) {
-      //   Future.delayed(const Duration(seconds: 2), () {
-      //     context.router.replaceAll([const SignInRoute()]);
-      //   });
-      // }
-    });
-  }
+class _SignInBody extends StatelessWidget {
+  const _SignInBody({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        alignment: Alignment.bottomCenter,
-        children: [
-          TabBarView(
-            controller: _tabController,
+      body: BlocListener<LoginBloc, LoginState>(
+        listener: (context, login) {
+          // if (login.authState == AuthStatus.signInError) {
+          //   ScaffoldMessenger.of(context)
+          //     ..hideCurrentSnackBar()
+          //     ..showSnackBar(
+          //       const SnackBar(content: Text('Authentication Failure')),
+          //     );
+          // }
+        },
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Column(
-                //  alignment: Alignment.topCenter,
-                children: [
-                  Transform.scale(
-                    scale: 1.4,
-                    child: Container(
-                      height: 240,
-                      transform: Matrix4.translationValues(0, -40, 0),
-                      decoration: BoxDecoration(
-                        color: Colors.indigo[900],
-                        borderRadius: BorderRadius.vertical(
-                            bottom: Radius.elliptical(
-                                MediaQuery.of(context).size.width, 160)),
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Form(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      TextFormField(
+                        onChanged: (email) => context
+                            .read<LoginBloc>()
+                            .add(LoginEmailChanged(email)),
+                        decoration: const InputDecoration(labelText: 'Email'),
+                        keyboardType: TextInputType.emailAddress,
                       ),
-                    ),
+                      const SizedBox(height: 16),
+                      TextFormField(
+                        onChanged: (pass) => context
+                            .read<LoginBloc>()
+                            .add(LoginPasswordChanged(pass)),
+                        decoration:
+                            const InputDecoration(labelText: 'Passeord'),
+                        keyboardType: TextInputType.visiblePassword,
+                      ),
+                      const SizedBox(height: 16),
+                      BlocBuilder<LoginBloc, LoginState>(
+                        builder: (context, state) {
+                          return LoadingButtonWidget(
+                            onPressed: () => context
+                                .read<LoginBloc>()
+                                .add(const LoginSubmitted()),
+                            label: 'Sign In',
+                            isLoading:
+                                state.authState == AuthStatus.authenticating
+                                    ? true
+                                    : false,
+                          );
+                        },
+                      ),
+                    ],
                   ),
-                  const Center(
-                    child: Text('Well Designed Template'),
-                  ),
-                ],
+                ),
               ),
-              Column(
-                //  alignment: Alignment.topCenter,
-                children: [
-                  Transform.scale(
-                    scale: 1.4,
-                    child: Container(
-                      height: 240,
-                      transform: Matrix4.translationValues(0, -40, 0),
-                      decoration: BoxDecoration(
-                        color: Colors.indigo[900],
-                        borderRadius: BorderRadius.vertical(
-                            bottom: Radius.elliptical(
-                                MediaQuery.of(context).size.width, 160)),
-                      ),
-                    ),
-                  ),
-                  const Center(
-                    child: Text('East to Export and Share'),
-                  ),
-                ],
-              ),
-              Column(
-                //  alignment: Alignment.topCenter,
-                children: [
-                  Transform.scale(
-                    scale: 1.4,
-                    child: Container(
-                      height: 240,
-                      transform: Matrix4.translationValues(0, -40, 0),
-                      decoration: BoxDecoration(
-                        color: Colors.indigo[900],
-                        borderRadius: BorderRadius.vertical(
-                            bottom: Radius.elliptical(
-                                MediaQuery.of(context).size.width, 160)),
-                      ),
-                    ),
-                  ),
-                  const Center(
-                    child: Text('Easy to Create and Edit'),
-                  ),
-                ],
+              const Center(
+                child: Text('SignInView'),
               ),
             ],
           ),
-          SafeArea(
-            child: Align(
-              alignment: Alignment.topRight,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextButton.icon(
-                  style: TextButton.styleFrom(
-                    primary: Colors.white,
-                  ),
-                  label: const Text('Skip'),
-                  icon: const Icon(Icons.arrow_forward),
-                  onPressed: () {
-                    context.router.replaceAll([const SignInRoute()]);
-                  },
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                TabPageSelector(
-                  controller: _tabController,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Visibility(
-                      visible: _tabController.index > 0,
-                      child: IconButton(
-                        icon: const Icon(Icons.arrow_back),
-                        onPressed: () {
-                          _tabController.animateTo(_tabController.index - 1);
-                        },
-                      ),
-                    ),
-                    Visibility(
-                      visible: _tabController.index < 2,
-                      child: IconButton(
-                        icon: const Icon(Icons.arrow_forward),
-                        onPressed: () {
-                          _tabController.animateTo(_tabController.index + 1);
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
