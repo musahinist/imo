70e91baba08dc6d7da79273bd12163341686d5e7
musahinist <sahinmusa1@gmail.com>
2022-04-13 21:12:15 +0300

    feat: add image utils

--- lib/src/config/constant/asset.dart
+++ lib/src/config/constant/asset.dart
@@ -1,8 +1,58 @@
+import 'dart:ui' as ui;
+import 'package:image/image.dart' as lib;
+import 'dart:typed_data';
+import 'package:flutter/services.dart';
+
 class Asset {
   const Asset._();
   static const audio = _AudioAsset();
   static const svg = _SvgAsset();
   static const image = _ImageAsset();
+  //ByteData opetaions
+  static Future<ByteData> loadImage() async {
+    ByteData data = await rootBundle.load('assets/image/octopus.png');
+    return data;
+  }
+
+  static Future<ui.Image> byteToUiImage(ByteData data) async {
+    final codec = await ui.instantiateImageCodec(data.buffer.asUint8List());
+    final frameInfo = await codec.getNextFrame();
+    return frameInfo.image;
+  }
+
+  static Future<lib.Image?> byteToLibImagePng(ByteData data) async {
+    return lib.decodePng(data.buffer.asUint8List());
+  }
+
+  static Uint8List byteToInt8List(ByteData data) {
+    return data.buffer.asUint8List();
+  }
+// Uint8list opeations
+
+  static Future<ui.Image> int8ListToUiImage(Uint8List list) async {
+    final codec = await ui.instantiateImageCodec(list);
+    final frameInfo = await codec.getNextFrame();
+    return frameInfo.image;
+  }
+
+  static Future<lib.Image?> int8ListToLibImage(Uint8List list) async {
+    return lib.decodePng(list);
+  }
+
+//Cross Operation
+  static Future<ui.Image> imgImageToUiImage(lib.Image image) async {
+    ui.ImmutableBuffer buffer = await ui.ImmutableBuffer.fromUint8List(
+        image.getBytes(format: lib.Format.rgba));
+    ui.ImageDescriptor id = ui.ImageDescriptor.raw(buffer,
+        height: image.height,
+        width: image.width,
+        pixelFormat: ui.PixelFormat.rgba8888);
+    ui.Codec codec = await id.instantiateCodec(
+        targetHeight: image.height, targetWidth: image.width);
+    ui.FrameInfo fi = await codec.getNextFrame();
+    ui.Image uiImage = fi.image;
+    return uiImage;
+  }
 }
 
 class _SvgAsset {
