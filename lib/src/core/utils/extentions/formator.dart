36fdaba887b47aee5269e4b4ead3716e0e1ae5d7
musahinist <sahinmusa1@gmail.com>
2022-05-03 04:23:10 +0300

    feat: add datetim ext

--- lib/src/core/utils/extentions/formator.dart
+++ lib/src/core/utils/extentions/formator.dart
@@ -15,6 +15,39 @@ extension HHmm on Duration {
   }
 }
 
+extension DateTimeExtension on DateTime {
+  String getMonthString() {
+    switch (month) {
+      case 1:
+        return 'Jan.';
+      case 2:
+        return 'Feb.';
+      case 3:
+        return 'Mar.';
+      case 4:
+        return 'Apr.';
+      case 5:
+        return 'May';
+      case 6:
+        return 'Jun.';
+      case 7:
+        return 'Jul.';
+      case 8:
+        return 'Aug.';
+      case 9:
+        return 'Sept.';
+      case 10:
+        return 'Oct.';
+      case 11:
+        return 'Nov.';
+      case 12:
+        return 'Dec.';
+      default:
+        return 'Err';
+    }
+  }
+}
+
 extension FormatNumber on int {
   String formatDecimalThousand() {
     //1403 -> 1,403
